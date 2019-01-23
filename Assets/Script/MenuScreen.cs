fdcebe183245eb7c117b6065c749159100884b29
sayali-deshpande <sjk126@gmail.com>
2019-01-23 23:33:54 +0530

    Added menu screen

---
+++ Assets/Script/MenuScreen.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuScreen : MonoBehaviour
+{
+  public void HidePanel()
+    {
+        this.gameObject.SetActive(false);
+    }
+}
